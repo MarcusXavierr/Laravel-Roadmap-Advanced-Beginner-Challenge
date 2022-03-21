cda50df8727f4299db1f420997569cba18486494
= <=>
2022-03-21 11:48:42 -0300

    create many to many relationship between user and project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -15,4 +15,9 @@ class Project extends Model
     {
         return $this->belongsTo(Client::class);
     }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
