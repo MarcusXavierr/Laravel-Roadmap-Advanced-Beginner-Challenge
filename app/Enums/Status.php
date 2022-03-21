964d260266c4f70dc38b2eed69c14a23e9693e31
= <=>
2022-03-21 12:28:34 -0300

    Create enum

---
+++ app/Enums/Status.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static OptionOne()
+ * @method static static OptionTwo()
+ * @method static static OptionThree()
+ */
+final class Status extends Enum
+{
+    const Open     = 1;
+    const Archived = 2;
+    const Finished = 3;
+}
