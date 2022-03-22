56babe67c59d7ac128040e6a63d1249838057a83
= <=>
2022-03-22 15:07:53 -0300

    create tests for index, store and update

--- tests/Feature/Http/Controllers/Api/v1/ClientControllerTest.php
+++ tests/Feature/Http/Controllers/Api/v1/ClientControllerTest.php
@@ -12,10 +12,16 @@ use Tests\TestCase;
 
 class ClientControllerTest extends ApiFeatureTestCase
 {
+
+    private array $payload = [
+        'name' => 'testing',
+        'address' => 'rua 1',
+        'phone_number' => '555-5555'
+    ];
     /** @test */
     public function guest_user_cannot_get_clients_index()
     {
-        $this->get(route('api.clients.index'))
+        $this->getJson(route('api.clients.index'))
             ->assertSee('Unauthenticated')
             ->assertStatus(401);
     }
@@ -24,19 +30,72 @@ class ClientControllerTest extends ApiFeatureTestCase
     public function common_user_cannot_see_listing()
     {
         $this->login();
-        $this->get(route('api.clients.index'))
+        $this->getJson(route('api.clients.index'))
             ->assertStatus(403);
     }
 
     /** @test */
     public function admin_can_see_client_index_listing()
     {
-        $user = User::factory()->create(['is_admin' => true]);
-        $this->login($user);
+        $this->loginAsAdmin();
         $clients = Client::factory(10)->create();
 
-        $this->get(route('api.clients.index'))
+        $this->getJson(route('api.clients.index'))
             ->assertOk()
             ->assertSee($clients[0]->name);
+    }
+
+    /** @test */
+    public function admin_can_create_a_client()
+    {
+        $this->loginAsAdmin();
+
+
+
+        $this->postJson(route('api.clients.store'), $this->payload)
+            ->assertStatus(201)
+            ->assertSee($this->payload['name']);
+
+        $this->assertDatabaseHas('clients', $this->payload);
+    }
+
+    /** @test */
+    public function missing_fields_on_store_method_should_fail()
+    {
+        $this->loginAsAdmin();
+
+        $this->postJson(route('api.clients.store'), [])
+            ->assertStatus(422)
+            ->assertJsonValidationErrors(['name', 'address', 'phone_number']);
+    }
+
+    /** @test */
+    public function admin_can_update_client()
+    {
+        $client = Client::factory()->create();
+        $this->loginAsAdmin();
+
+        $this->putJson(
+            route('api.clients.update', ['client' => $client->id]),
+            $this->payload
+        )
+            ->assertStatus(201)
+            ->assertSee($this->payload['name']);
+
+        $this->assertDatabaseHas('clients', ['name' => $this->payload['name']]);
+    }
+
+    /** @test */
+    public function admin_cannot_update_to_a_existing_name()
+    {
+        $client = Client::factory()->create();
+        Client::factory()->create(['name' => 'myname']);
+        $this->loginAsAdmin();
+
+        $this->putJson(
+            route('api.clients.update', ['client' => $client->id]),
+            ['name' => 'myname']
+        )
+            ->assertJsonValidationErrors(['name']);
     }
 }
