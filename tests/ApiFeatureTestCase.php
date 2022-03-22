cac3d0cc1b0fcbdc8a6c74443f545fac6753e0dc
= <=>
2022-03-22 15:09:36 -0300

    remove unused method

--- tests/ApiFeatureTestCase.php
+++ tests/ApiFeatureTestCase.php
@@ -4,9 +4,4 @@ namespace Tests;
 
 class ApiFeatureTestCase extends FeatureTestCase
 {
-    public function get($uri, array $headers =
-    ['Content-Type' => 'application/json', 'Accept' => 'application/json'])
-    {
-        return parent::get($uri, $headers);
-    }
 }
