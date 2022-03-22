d37c90946f6412abd8937506f744b8d960126065
= <=>
2022-03-22 15:23:07 -0300

    Create authController and test classes

---
+++ tests/Feature/Http/Controllers/Api/v1/AuthControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api\v1;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\ApiFeatureTestCase;
+use Tests\TestCase;
+
+class AuthControllerTest extends ApiFeatureTestCase
+{
+}
