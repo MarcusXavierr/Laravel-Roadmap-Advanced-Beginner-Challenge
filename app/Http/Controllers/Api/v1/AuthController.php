84e5f0f20be691932b4364e63bba36c4c6ddb6ab
= <=>
2022-03-22 15:43:20 -0300

    Create register method and test

--- app/Http/Controllers/Api/v1/AuthController.php
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -3,8 +3,37 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+    public function register(Request $request)
+    {
+        $fields = $request->validate(
+            [
+                'name' => 'required|string',
+                'email' => 'required|string|unique:users,email',
+                'password' => 'required|string'
+            ]
+        );
+
+
+        $user = User::create(
+            [
+                'name' => $fields['name'],
+                'email' =>  $fields['email'],
+                'password' => bcrypt($fields['password'])
+            ]
+        );
+
+        $token = $user->createToken('mytoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
 }
--- tests/Feature/Http/Controllers/Api/v1/AuthControllerTest.php
+++ tests/Feature/Http/Controllers/Api/v1/AuthControllerTest.php
@@ -4,9 +4,32 @@ namespace Tests\Feature\Http\Controllers\Api\v1;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\ApiFeatureTestCase;
 use Tests\TestCase;
 
 class AuthControllerTest extends ApiFeatureTestCase
 {
+
+    /** @test */
+    public function user_can_register()
+    {
+        $payload = [
+            'name' => 'Marcus',
+            'email' => 'marcus.xavier@aluno.ufop.edu.br',
+            'password' => 'password',
+        ];
+        $this->postJson(route('api.register'), $payload)
+            ->assertStatus(201)
+            ->assertSeeText('token');
+
+        $this->assertDatabaseHas('users', ['name' => $payload['name'], 'email' => $payload['email']]);
+    }
+
+    /** @test */
+    public function user_gets_error_message_when_not_validate_request()
+    {
+        $this->postJson(route('api.register'), [])
+            ->assertJsonValidationErrors(['name', 'email', 'password']);
+    }
 }
