febde39007724deb58eab921a66789b7d0c2dc2e
= <=>
2022-03-22 15:08:08 -0300

    create index, store and update methods

--- app/Http/Controllers/Api/v1/ClientController.php
+++ app/Http/Controllers/Api/v1/ClientController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\StoreClientResource;
+use App\Http\Resources\ClientsResource;
+use App\Models\Client;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
@@ -14,7 +17,7 @@ class ClientController extends Controller
      */
     public function index()
     {
-        //
+        return ClientsResource::collection(Client::all());
     }
 
     /**
@@ -23,9 +26,11 @@ class ClientController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreClientResource $request)
     {
-        //
+        $data = $request->validated();
+        $client = Client::create($data);
+        return new ClientsResource($client);
     }
 
     /**
@@ -48,7 +53,10 @@ class ClientController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate(['name' => 'unique:clients']);
+        $client = Client::findOrFail($id);
+        $client->update($request->all());
+        return response(new ClientsResource($client), 201);
     }
 
     /**
