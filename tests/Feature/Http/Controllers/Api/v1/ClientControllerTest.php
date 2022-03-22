6e7260fc16a1d90f713d2d85ef092b618f87e5a7
= <=>
2022-03-22 13:41:20 -0300

    Create first tests on ApiClientController

--- tests/Feature/Http/Controllers/Api/v1/ClientControllerTest.php
+++ tests/Feature/Http/Controllers/Api/v1/ClientControllerTest.php
@@ -2,21 +2,41 @@
 
 namespace Tests\Feature\Http\Controllers\Api\v1;
 
+use App\Models\Client;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Tests\ApiFeatureTestCase;
+use Tests\FeatureTestCase;
 use Tests\TestCase;
 
-class ClientControllerTest extends TestCase
+class ClientControllerTest extends ApiFeatureTestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    /** @test */
+    public function guest_user_cannot_get_clients_index()
     {
-        $response = $this->get('/');
+        $this->get(route('api.clients.index'))
+            ->assertSee('Unauthenticated')
+            ->assertStatus(401);
+    }
 
-        $response->assertStatus(200);
+    /** @test */
+    public function common_user_cannot_see_listing()
+    {
+        $this->login();
+        $this->get(route('api.clients.index'))
+            ->assertStatus(403);
+    }
+
+    /** @test */
+    public function admin_can_see_client_index_listing()
+    {
+        $user = User::factory()->create(['is_admin' => true]);
+        $this->login($user);
+        $clients = Client::factory(10)->create();
+
+        $this->get(route('api.clients.index'))
+            ->assertOk()
+            ->assertSee($clients[0]->name);
     }
 }
